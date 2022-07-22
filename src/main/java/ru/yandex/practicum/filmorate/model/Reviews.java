c83b90b7583fddd57533294a07cfd019c9914a70
Nabokov79 <nabokovsg@yandex.ru>
2022-07-22 21:31:30 +0300

    add Revies model

--- src/main/java/ru/yandex/practicum/filmorate/model/Reviews.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Reviews.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class Reviews {
 
@@ -14,12 +16,4 @@ public class Reviews {
     private int filmId;
     private int useful;
 
-    public Reviews(int reviewId, String content, boolean isPositive, int userId, int filmId, int useful) {
-        this.reviewId = reviewId;
-        this.content = content;
-        this.isPositive = isPositive;
-        this.userId = userId;
-        this.filmId = filmId;
-        this.useful = useful;
-    }
 }
