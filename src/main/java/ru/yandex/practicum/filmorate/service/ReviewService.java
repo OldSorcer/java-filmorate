cbd354645ac2dc480a3a852bcdc8287117fa408e
Nabokov79 <nabokovsg@yandex.ru>
2022-07-28 19:40:54 +0300

    add ReviewDaoService

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -3,74 +3,74 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.model.Reviews;
-import ru.yandex.practicum.filmorate.storage.film.dao.ReviewsDao;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.storage.film.dao.ReviewDao;
 import ru.yandex.practicum.filmorate.validator.Validator;
 import java.util.List;
 
 @Service
 @Slf4j
-public class ReviewsService {
+public class ReviewService {
 
-    private final ReviewsDao reviewsDao;
+    private final ReviewDao reviewsDao;
     private final UserService userService;
     private  final FilmService filmService;
 
     @Autowired
-    public ReviewsService(ReviewsDao reviewsDao, UserService userService, FilmService filmService) {
+    public ReviewService(ReviewDao reviewsDao, UserService userService, FilmService filmService) {
         this.reviewsDao = reviewsDao;
         this.userService = userService;
         this.filmService = filmService;
     }
 
-    public Reviews addReviews(Reviews reviews) {
-        Validator.validateReview(reviews);
-        userService.getUserById(reviews.getUserId());
-        filmService.getFilmById(reviews.getFilmId());
-        reviewsDao.addReviews(reviews);
-        return reviews;
+    public Review addReview(Review review) {
+        Validator.validateReview(review);
+        userService.getUserById(review.getUserId());
+        filmService.getFilmById(review.getFilmId());
+        reviewsDao.addReview(review);
+        return review;
     }
 
-    public Reviews updateReviews(Reviews reviews) {
-        getReviewsById(reviews.getReviewId());
-        reviewsDao.updateReviews(reviews);
-        return getReviewsById(reviews.getReviewId());
+    public Review updateReview(Review review) {
+        getReviewById(review.getReviewId());
+        reviewsDao.updateReview(review);
+        return getReviewById(review.getReviewId());
     }
 
-    public Reviews getReviewsById(int reviewId) {
+    public Review getReviewById(int reviewId) {
         return reviewsDao.getReviewById(reviewId);
     }
 
-    public void deleteReviews(int id) {
-        getReviewsById(id);
-        reviewsDao.deleteReviews(id);
+    public void deleteReview(int id) {
+        getReviewById(id);
+        reviewsDao.deleteReview(id);
     }
 
     public void addReviewLike(int id, int userId) {
-        getReviewsById(id);
+        getReviewById(id);
         userService.getUserById(userId);
         reviewsDao.addReviewLike(id, userId);
     }
 
     public void addReviewDislike(int id, int userId) {
-        getReviewsById(id);
+        getReviewById(id);
         userService.getUserById(userId);
         reviewsDao.addReviewDislike(id, userId);
     }
 
     public void deleteReviewLike(int id, int userId) {
-        getReviewsById(id);
+        getReviewById(id);
         userService.getUserById(userId);
         reviewsDao.deleteReviewLike(id, userId);
     }
 
     public void deleteReviewDislike(int id, int userId) {
-        getReviewsById(id);
+        getReviewById(id);
         userService.getUserById(userId);
         reviewsDao.deleteReviewDislike(id, userId);
     }
 
-    public List<Reviews> getAllReviewsByFilmId(int filmId, int count) {
+    public List<Review> getAllReviewsByFilmId(int filmId, int count) {
         if (filmId == 0) {
             return reviewsDao.getAllReviews();
         } else {
