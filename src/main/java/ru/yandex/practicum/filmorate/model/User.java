83417447376d367c80964da2b4fc6e03995b9b38
Nabokov79 <nabokovsg@yandex.ru>
2022-07-28 21:13:32 +0300

    add madel

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,15 +1,10 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -21,5 +16,4 @@ public class User {
     private String email;
     private int id;
     private LocalDate birthday;
-    private Set<Integer> friendsList = new HashSet<>();
 }
