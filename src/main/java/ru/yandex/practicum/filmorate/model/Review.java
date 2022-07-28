1faa74c29e4da004259349a5a939ae45ed1e6c4b
Nabokov79 <nabokovsg@yandex.ru>
2022-07-28 19:39:14 +0300

    add new model

--- src/main/java/ru/yandex/practicum/filmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Reviews {
+public class Review {
 
     private int reviewId;
     private String content;
