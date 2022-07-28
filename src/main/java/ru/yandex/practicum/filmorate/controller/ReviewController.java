5b4c3d38b63d2463ca6ef5ab29c1183f12e4eeaf
Nabokov79 <nabokovsg@yandex.ru>
2022-07-28 19:39:50 +0300

    add ReviewController

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewController.java
@@ -5,77 +5,77 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Reviews;
-import ru.yandex.practicum.filmorate.service.ReviewsService;
+import ru.yandex.practicum.filmorate.model.Review;
+import ru.yandex.practicum.filmorate.service.ReviewService;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/reviews")
 @Slf4j
-public class ReviewsController {
-    private final ReviewsService reviewsService;
+public class ReviewController {
+    private final ReviewService reviewsService;
 
     @Autowired
-    public ReviewsController(ReviewsService reviewsService) {
+    public ReviewController(ReviewService reviewsService) {
         this.reviewsService = reviewsService;
     }
 
     @PostMapping
-    public ResponseEntity<Reviews> addReviews(@RequestBody Reviews reviews) {
+    public ResponseEntity<Review> addReview (@RequestBody Review reviews) {
         log.info("Получен POST запрос к эндпоинту /reviews");
-        return ResponseEntity.ok().body(reviewsService.addReviews(reviews));
+        return ResponseEntity.ok().body(reviewsService.addReview(reviews));
     }
 
     @PutMapping
-    public ResponseEntity<Reviews> updateReviews(@RequestBody Reviews reviews) {
+    public ResponseEntity<Review> updateReview (@RequestBody Review review) {
         log.info("Получен PUT запрос к эндпоинту /reviews");
-        return ResponseEntity.ok().body(reviewsService.updateReviews(reviews));
+        return ResponseEntity.ok().body(reviewsService.updateReview(review));
     }
 
     @GetMapping("/{reviewId}")
-    public ResponseEntity<Reviews> getReviewsById (@PathVariable int reviewId) {
+    public ResponseEntity<Review> getReviewById (@PathVariable int reviewId) {
         log.info("Получен GET запрос к эндпоинту /reviews");
-        return ResponseEntity.ok().body(reviewsService.getReviewsById(reviewId));
+        return ResponseEntity.ok().body(reviewsService.getReviewById(reviewId));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Reviews> deleteReviews (@PathVariable int id) {
+    public ResponseEntity<Review> deleteReviews (@PathVariable int id) {
         log.info("Получен DELETE запрос к эндпоинту /reviews/{id}");
-        reviewsService.deleteReviews(id);
+        reviewsService.deleteReview(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}/like/{userId}")
-    public ResponseEntity<Reviews> addReviewLike(@PathVariable int id, @PathVariable int userId) {
+    public ResponseEntity<Review> addReviewLike(@PathVariable int id, @PathVariable int userId) {
         log.info("Получен PUT запрос к эндпоинту /reviews/{id}/like/{userId}");
         reviewsService.addReviewLike(id, userId);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{id}/dislike/{userId}")
-    public ResponseEntity<Reviews> addReviewDislike(@PathVariable int id, @PathVariable int userId) {
+    public ResponseEntity<Review> addReviewDislike(@PathVariable int id, @PathVariable int userId) {
         log.info("Получен PUT запрос к эндпоинту /reviews/{id}/dislike/{userId}");
         reviewsService.addReviewDislike(id, userId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/like/{userId}")
-    public ResponseEntity<Reviews> deleteReviewLike(@PathVariable int id, @PathVariable int userId) {
+    public ResponseEntity<Review> deleteReviewLike(@PathVariable int id, @PathVariable int userId) {
         log.info("Получен DELETE запрос к эндпоинту /reviews/{id}/like/{userId}");
         reviewsService.deleteReviewLike(id, userId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}/dislike/{userId}")
-    public ResponseEntity<Reviews> deleteReviewDislike (@PathVariable int id, @PathVariable int userId) {
+    public ResponseEntity<Review> deleteReviewDislike (@PathVariable int id, @PathVariable int userId) {
         log.info("Получен DELETE запрос к эндпоинту /reviews/{id}/dislike/{userId}");
         reviewsService.deleteReviewDislike(id, userId);
         return ResponseEntity.ok().build();
     }
 
    @GetMapping
-    public ResponseEntity<List<Reviews>> getAllReviewsByFilmId (
+    public ResponseEntity<List<Review>> getAllReviewsByFilmId (
             @RequestParam(value = "filmId", defaultValue = "0")  int filmId,
             @RequestParam(value = "count", defaultValue = "10") int count
    ) {
